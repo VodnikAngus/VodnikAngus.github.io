fed57261227a07d285292a143cb9e97cc9a8a0c9
Vodnik <petar.radojevic22@gmail.com>
2020-01-09 13:28:35 +0100

    bubblesssssssss

--- scripts/bubbles.js
+++ scripts/bubbles.js
@@ -2,18 +2,40 @@ let home = document.getElementById("home");
 let bubblesDiv = document.getElementById("bubbles");
 let bubbles = [];
 
+function createBubble() {
+  let bubble = document.createElement("div");
+  bubble.classList.add("bubble");
+  bubble.R = 50 + Math.random(100) * 70;
+  bubble.style.setProperty("--R", bubble.R);
+  bubble.style.setProperty("--left", Math.random());
+  bubble.style.setProperty("--top", Math.random());
+  bubble.style.setProperty("--opacity", Math.random() / 2 + 0.1);
+  bubble.style.setProperty("--speed", 0.5 + Math.random() / 2);
+  bubble.style.filter = `blur(${Math.random() * 3}px)`;
+  bubblesDiv.appendChild(bubble);
+  return bubble;
+}
+
 window.onload = () => {
-  for (let i = 0; i < 15; i++) {
-    bubbles[i] = document.createElement("div");
-    bubbles[i].classList.add("bubble");
-    bubbles[i].R = 50 + Math.random(100) * 70;
-    bubbles[i].style.setProperty("--R", bubbles[i].R);
-    bubbles[i].style.setProperty("--left", Math.random());
-    let top = Math.random();
-    bubbles[i].style.setProperty("--top", top);
-    bubbles[i].style.opacity = Math.random() / 2 + 0.1;
-    bubbles[i].style.filter = `blur(${Math.random() * 3}px)`;
-    bubbles[i].style.setProperty("--speed", 0.5 + Math.random() / 2);
-    bubblesDiv.appendChild(bubbles[i]);
+  nBubbles = Math.ceil(bubblesDiv.offsetWidth / 100);
+  console.log(nBubbles);
+  for (let i = 0; i <= nBubbles; i++) {
+    bubbles = [...bubbles, createBubble()];
   }
+  new ResizeObserver(bubblesResized).observe(bubblesDiv);
 };
+
+function bubblesResized() {
+  let newBubbles = Math.ceil(bubblesDiv.offsetWidth / 100);
+  if (newBubbles <= nBubbles) {
+    for (let index = 0; index < bubbles.length; index++) {
+      bubbles[index].style.opacity = index < newBubbles ? null : 0;
+    }
+    return;
+  }
+  console.log(`${nBubbles} ${newBubbles}`);
+  for (let index = 0; index < newBubbles - nBubbles; index++) {
+    bubbles = [...bubbles, createBubble()];
+  }
+  nBubbles = newBubbles;
+}
