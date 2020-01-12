baed2610355c8a8b805627d3b47b66559a6e014e
Vodnik <petar.radojevic22@gmail.com>
2020-01-12 22:28:38 +0100

    much design much wow?

--- scripts/bubbles.js
+++ scripts/bubbles.js
@@ -5,19 +5,19 @@ let bubbles = [];
 function createBubble() {
   let bubble = document.createElement("div");
   bubble.classList.add("bubble");
-  bubble.R = 50 + Math.random(100) * 70;
+  bubble.R = 120 + Math.random(100) * 140;
   bubble.style.setProperty("--R", bubble.R);
   bubble.style.setProperty("--left", Math.random());
   bubble.style.setProperty("--top", Math.random());
   bubble.style.setProperty("--opacity", Math.random() / 2 + 0.1);
   bubble.style.setProperty("--speed", 0.5 + Math.random() / 2);
-  bubble.style.filter = `blur(${Math.random() * 3}px)`;
+  //bubble.style.filter = `blur(${Math.random() * 3}px)`;
   bubblesDiv.appendChild(bubble);
   return bubble;
 }
 
 window.onload = () => {
-  nBubbles = Math.ceil(bubblesDiv.offsetWidth / 100);
+  nBubbles = 3 + Math.ceil(bubblesDiv.offsetWidth / 250);
 
   for (let i = 0; i <= nBubbles; i++) {
     bubbles = [...bubbles, createBubble()];
@@ -26,7 +26,7 @@ window.onload = () => {
 };
 
 function bubblesResized() {
-  let newBubbles = Math.ceil(bubblesDiv.offsetWidth / 100);
+  let newBubbles = 3 + Math.ceil(bubblesDiv.offsetWidth / 250);
 
   if (newBubbles <= nBubbles) {
     for (let index = 0; index < bubbles.length; index++) {
